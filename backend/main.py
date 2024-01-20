f5a5c0e461acb7ebf097a01ede75b89129b5e676
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2024-01-20 14:09:20 +0300

    add routers to main router

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.routing import APIRouter
 
+from api.handlers.users.handlers import user_router
+from api.handlers.auth.handlers import auth_router
 
 app = FastAPI(title="SystemBonus")
 
@@ -15,4 +17,6 @@ app.add_middleware(
 )
 
 main_api_router = APIRouter()
+main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+main_api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(main_api_router)
