c55127462110b6b92e6ebb5e95ed7862fc197521
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2024-01-21 01:44:39 +0300

    add action create profile

--- backend/api/actions/users.py
+++ backend/api/actions/users.py
@@ -1,9 +1,8 @@
 import uuid
 
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from api.handlers.users.schemas import CreatedUser, UpdatedUser
-from db.users.dals import UserDAL
+from api.handlers.users.schemas import CreatedUser, UpdatedUser, ShowProfileUser
+from db.users.dals import UserDAL, ProfileDAL
 from hashing import Hasher
 
 
@@ -15,9 +14,19 @@ async def _create_user(phone: str, password: str, session: AsyncSession) -> Crea
             return CreatedUser(user_id=user.user_id)
 
 
-async def _update_user(user_id: uuid.UUID, is_active: bool, is_verified_phone: bool, session: AsyncSession) -> UpdatedUser | None:
+async def _update_user(user_id: uuid.UUID, is_active: bool, is_verified_phone: bool,
+                       session: AsyncSession) -> UpdatedUser | None:
     async with session.begin():
         user_dal = UserDAL(session)
         user_id = await user_dal.update_user(user_id=user_id, is_active=is_active, is_verified_phone=is_verified_phone)
         if user_id:
             return UpdatedUser(user_id=user_id)
+
+
+async def _create_profile(user_id: uuid.UUID, phone: str, first_name: str, last_name: str, session: AsyncSession) -> ShowProfileUser | None:
+    async with session.begin():
+        profile_dal = ProfileDAL(session)
+        profile = await profile_dal.create_profile(user_id=user_id, first_name=first_name, last_name=last_name)
+        if profile:
+            return ShowProfileUser(phone=phone, first_name=profile.first_name,
+                                   last_name=profile.last_name, bonuses=profile.bonuses)
