cbe7d784af68b9fa76c36a8d37bc6a2a233b67c1
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2024-01-21 00:34:54 +0300

    add profile model

--- backend/db/users/models.py
+++ backend/db/users/models.py
@@ -1,7 +1,7 @@
 import uuid
 from db.base import Base
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped
+from sqlalchemy import String, Boolean, ForeignKey, BigInteger
+from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.orm import mapped_column
 
 
@@ -13,3 +13,16 @@ class User(Base):
     password: Mapped[str]
     is_active: Mapped[bool] = mapped_column(Boolean, default=False)
     is_verified_phone: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    profile: Mapped["Profile"] = relationship("Profile", back_populates="user")
+
+
+class Profile(Base):
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.user_id", ondelete="CASCADE"), unique=True)
+    first_name: Mapped[str] = mapped_column(String(20))
+    last_name: Mapped[str] = mapped_column(String(40))
+    bonuses: Mapped[int] = mapped_column(BigInteger)
+
+    user: Mapped[User] = relationship(User, back_populates="profile")
+
