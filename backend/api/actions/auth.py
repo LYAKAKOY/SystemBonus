79eb33f55e858ccc485f355ee0a1d8bfe926dca9
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2024-01-20 14:08:02 +0300

    add func get current user

--- backend/api/actions/auth.py
+++ backend/api/actions/auth.py
@@ -1,20 +1,28 @@
 import json
+import random
 import uuid
 
 import aiohttp
 from aiohttp.web_response import Response
 
 import settings
-from api.handlers.schemas import CreatedUser
+from api.handlers.users.schemas import ShowUser
+from db.session import get_db
 from db.users.models import User
-from db.users.user_dal import UserDAL
-from fastapi import HTTPException
+from db.users.dals import UserDAL
+from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from hashing import Hasher
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
+from jwt_auth import JWT
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
+
+def generate_code() -> str:
+    confirmation_code = "".join(random.choice("0123456789") for _ in range(4))
+    return confirmation_code
 
 
 async def _get_user_by_user_id_for_auth(
@@ -25,7 +33,7 @@ async def _get_user_by_user_id_for_auth(
         return await user_dal.get_user_by_user_id(user_id=user_id)
 
 
-async def _get_user_by_login_for_auth(phone: str, session: AsyncSession) -> User | None:
+async def _get_user_by_phone_for_auth(phone: str, session: AsyncSession) -> User | None:
     async with session.begin():
         user_dal = UserDAL(session)
         return await user_dal.get_user_by_phone(phone=phone)
@@ -36,7 +44,7 @@ async def authenticate_user(phone: str, password: str, db: AsyncSession) -> User
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Incorrect username or password",
     )
-    if (user := await _get_user_by_login_for_auth(phone=phone, session=db)) is None:
+    if (user := await _get_user_by_phone_for_auth(phone=phone, session=db)) is None:
         raise credentials_exception
     if not Hasher.verify_password(password, user.password):
         raise credentials_exception
@@ -62,10 +70,12 @@ async def send_message_to_whatsapp(phone: str, message: str) -> Response:
             return response
 
 
-async def _create_user(phone: str, password: str, session: AsyncSession) -> CreatedUser | None:
-    async with session.begin():
-        user_dal = UserDAL(session)
-        user = await user_dal.create_user(phone=phone, password=Hasher.get_password_hash(password))
-        if user:
-            return CreatedUser(user_id=user.user_id)
-
+async def get_current_user_from_token(
+        token: str = Depends(oauth2_scheme),
+        db: AsyncSession = Depends(get_db)
+) -> ShowUser:
+    decoded = JWT.decode_jwt(token=token)
+    user_id = decoded.get("sub")
+    user = await _get_user_by_user_id_for_auth(user_id=user_id, session=db)
+    if user is not None:
+        return user
